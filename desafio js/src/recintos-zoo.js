01eb753c9731a380b10f813f42d4ae0ea4891c34
IndustriaMentis <147935382+IndustriaMentis@users.noreply.github.com>
2024-09-13 00:04:11 -0300

    Add files via upload

---
+++ desafio js/src/recintos-zoo.js
@@ -0,0 +1,29 @@
+class RecintosZoo {
+
+    analisaRecintos(animal, quantidade) {
+    }
+
+}
+
+export { RecintosZoo as RecintosZoo };
+//Entendo que este é um projeto importante. Mas, por ele usar de estruturas condicionais, classes e outros conceitos bem complexos, vai ficar incompleta a sua resolução.
+//Fiz o melhor que pude no tempo que tive.
+class animais{
+    especies = ["LEAO", "savana", 3]; 
+    ["LEOPARDO", "savana", 2]; 
+    ["CROCODILO", "rio", 3];
+    ["MACACO","savana ou floresta", 1];
+    ["GAZELA","savana", 2];
+    ["HIPOPOTAMO","savana ou rio", 4]
+}
+
+class Bioma{
+    savana = 1
+    floresta = 5
+    savanaerio = 3
+    rio = 8
+    savana = 5
+
+}
+
+new RecintosZoo(savana).analisaRecintos("MACACO", 2)
